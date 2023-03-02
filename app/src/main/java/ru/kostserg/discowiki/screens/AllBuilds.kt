818dcecdbd9cc6530907911894e5d7433ae2c89a
Z4ratul <dudemanik13@gmail.com>
2023-03-02 14:34:26 +0500

    New screens and navigation

--- app/src/main/java/ru/kostserg/discowiki/screens/AllBuilds.kt
+++ app/src/main/java/ru/kostserg/discowiki/screens/AllBuilds.kt
@@ -1,8 +1,65 @@
 package ru.kostserg.discowiki.screens
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.List
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import ru.kostserg.discowiki.navigation.WikiRoute
+import ru.kostserg.discowiki.ui.theme.DiscoWikiTheme
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
-fun AllBuilds (navController: NavHostController){
+fun AllBuildsScreen (navController: NavHostController){
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(onClick = { navController.navigate(WikiRoute.Build.route) })
+            {
+                Icon(Icons.Filled.Add, contentDescription = "Новый билд")
+            }
+        }
+    ) {
+        Column() {
+            BuildItem(name = "Build 1", navController = navController)
+            BuildItem(name = "Build 2", navController = navController)        }
+        }
+}
+@Composable
+fun BuildItem(name:String, navController:NavHostController){
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 10.dp, horizontal = 10.dp)
+            .clickable {
+                navController.navigate(WikiRoute.Build.route)
+            }
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(text = name,
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Bold,
+
+                )
+        }
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun prevAllBuildsScreen(){
+    DiscoWikiTheme() {
+        MainScreen(navController = rememberNavController())
+    }
 }
--- app/src/main/java/ru/kostserg/discowiki/screens/Build.kt
+++ app/src/main/java/ru/kostserg/discowiki/screens/Build.kt
@@ -1,8 +1,21 @@
 package ru.kostserg.discowiki.screens
 
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import ru.kostserg.discowiki.ui.theme.DiscoWikiTheme
 
 @Composable
-fun Build (navController: NavHostController){
+fun BuildScreen (navController: NavHostController){
+    Text(text = "DADAD")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun prevBuildScreen(){
+    DiscoWikiTheme() {
+        BuildScreen(navController = rememberNavController())
+    }
 }
